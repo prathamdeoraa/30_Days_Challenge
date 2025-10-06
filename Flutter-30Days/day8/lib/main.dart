f89870a7f7a7ea95388bc7a2dbaec0b254256cb0
Pratham Deora <prathamdeora@Prathams-MacBook-Air.local>
2025-10-06 18:03:40 +0530

    MESAGE

--- Flutter-30Days/day8/lib/main.dart
+++ Flutter-30Days/day8/lib/main.dart
@@ -9,10 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Homepage(),
-      debugShowCheckedModeBanner: false,
-    );
+    return MaterialApp(home: Homepage(), debugShowCheckedModeBanner: false);
   }
 }
 
@@ -22,7 +19,126 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
+      backgroundColor: Colors.white,
+
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70),
+
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blue, Colors.purple],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          title: Text(
+            "Flutter_DAY-8",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+
+          backgroundColor: Colors.blue,
+        ),
+      ),
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: 100,
+            child: FloatingActionButton(
+              heroTag: null,
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FirstPage()),
+                ),
+              },
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+
+              child: Text("List View"),
+            ),
+          ),
+          SizedBox(height: 10),
+
+          Container(
+            width: 100,
+            child: FloatingActionButton(
+              heroTag: null,
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                ),
+              },
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+
+              child: Text("Grid View"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  FirstPage({super.key});
+  final List<String> items = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("ListView Example")),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.food_bank),
+            title: Text(items[index]),
+            trailing: Icon(Icons.arrow_forward),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  SecondPage({super.key});
+
+  final List<String> items = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("ListView Example")),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ),
+        itemCount: 6,
+        itemBuilder: (context, index) {
+          return Container(
+            color: Colors.blueAccent,
+            child: Center(
+              child: Text(
+                "Item ${index + 1}",
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
